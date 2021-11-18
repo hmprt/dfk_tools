4278ba38f562748ee2e5c644d4c0adc1c85875b6
hmprt <harvey@atomicsear.ch>
2021-11-18 00:18:46 -0500

    Iniital commit. Can track hero stamina from CLI

--- get_char_stats.py
+++ get_char_stats.py
@@ -1,5 +1,58 @@
 """
 A script to get my Hero's stamina from the CLI
 """
-pass
+import requests
+import os
+import json
+import time
+import math
+from typing import List
 
+# endpoint
+url = "https://graph2.defikingdoms.com/subgraphs/name/defikingdoms/apiv5"
+
+# loading queries and params from files
+pwd = os.path.dirname(os.path.realpath(__file__))
+queries_folder = pwd + "/queries"
+
+# creating query variables programatically
+for query in os.listdir(queries_folder):
+    for data in os.listdir(queries_folder + "/" + query):
+        path = queries_folder + "/" + query + "/" + data
+        if "params" in data:
+            globals()[f"{query}_params"] = json.load(open(path))
+        if "query" in data:
+            globals()[f"{query}_query"] = open(path).read()
+
+
+def get_all_heroes(address: str) -> List[dict]:
+    """
+    Returns all heroes for a given user
+    """
+    query = get_all_heroes_query
+    params = get_all_heroes_params
+    params["owner"] = address
+
+    return json.loads(
+        requests.post(url, data=json.dumps({"query": query, "variables": params})).text
+    )["data"]["heros"]
+
+
+def get_heroes_and_stamina(address: str) -> List[dict]:
+    return [
+        {
+            "hero_id": hero["id"],
+            "time_until_stamina_full": str(
+                math.ceil((int(hero["staminaFullAt"]) - int(time.time())) / 60)
+            )
+            + " minutes",
+        }
+        for hero in get_all_heroes(address)
+    ]
+
+
+if __name__ == "__main__":
+    heroes = get_heroes_and_stamina(os.environ["DFK_ADDRESS"])
+    from pprint import pprint
+    for hero in heroes:
+        pprint(hero)
